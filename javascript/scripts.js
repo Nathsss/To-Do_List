96eec14bcba33d8b110c1dd55f63daaa7e67f222
Nathsss <rokirotl50@gmail.com>
2024-08-31 00:50:57 -0400

    criando primeiras variaveis

---
+++ javascript/scripts.js
@@ -0,0 +1,9 @@
+let card = document.getElementsByClassName("card")
+let inputTarefa = document.getElementsByClassName("form-control")
+let 
+
+var msgArray = new Array();
+msgArray[0] = "Hello";
+msgArray[99] = "world";
+
+if (msgArray.length == 100) console.log("O length é 100.");
